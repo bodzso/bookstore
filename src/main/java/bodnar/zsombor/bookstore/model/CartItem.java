206dd8c362ed56e18ff2462176eb9ad99cf8869e
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2020-05-05 15:37:43 +0200

    Add missing Entity annotation to CartItem

--- src/main/java/bodnar/zsombor/bookstore/model/CartItem.java
+++ src/main/java/bodnar/zsombor/bookstore/model/CartItem.java
@@ -1,5 +1,6 @@
 package bodnar.zsombor.bookstore.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
