d455f1f1c18eac43b5c8bf1595a3cb80b054accc
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2020-05-05 17:29:36 +0200

    Implement user creation

---
+++ src/main/java/bodnar/zsombor/bookstore/service/UserService.java
@@ -0,0 +1,27 @@
+package bodnar.zsombor.bookstore.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import bodnar.zsombor.bookstore.model.User;
+import bodnar.zsombor.bookstore.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Transactional
+	public Long createUser(User user)
+	{
+		return userRepository.save(user).getId();
+	}
+
+	public List<User> findAll() {
+		return (List<User>) userRepository.findAll();
+	}
+}
